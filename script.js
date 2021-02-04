fd79bddb3f0cb8dc58f9689dfe7ad51b3bbf7d8f
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2021-02-04 12:43:14 -0700

    finished game logic

--- script.js
+++ script.js
@@ -8,15 +8,17 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const SecretNumber = Math.trunc(Math.random()*20) +1;
+let SecretNumber = Math.trunc(Math.random()*20) +1;
 
 let score = 20; 
-document.querySelector('.number').textContent = SecretNumber; 
+let highScore = 0;
+
 
 document.querySelector('.check').addEventListener 
 ('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess);
+
 
     // When no input
     if (!guess) {
@@ -28,7 +30,13 @@ document.querySelector('.check').addEventListener
         document.querySelector('.message').textContent = 'Correct Number!';
 
         document.querySelector('body').style.backgroundColor = '#4b7a47';
-    
+        document.querySelector('.number').textContent = SecretNumber; 
+        document.querySelector('.number').style.width = '30rem';
+
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore; 
+        }
     // When guess is too high
     } else if (guess > SecretNumber) {
         if (score > 1 ) {
@@ -53,3 +61,15 @@ document.querySelector('.check').addEventListener
 
 });
 
+document.querySelector('.again').addEventListener
+('click', function() {
+    score = 20; 
+    SecretNumber = Math.trunc(Math.random()*20) +1;
+    
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+})
